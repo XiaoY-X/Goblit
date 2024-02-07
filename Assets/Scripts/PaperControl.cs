6679525115cd5fcdeb9ac8ad9eca3c02827d658b
Iván Caraballo Mesa <guativan@gmail.com>
2024-02-08 00:16:01 +0100

    Revert "Delete Assets directory"
    This reverts commit 496f719521c3698d686140cd08e404b5b40e32ef.

---
+++ Assets/Scripts/PaperControl.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaperControl : MonoBehaviour
+{
+    private bool paperIsCatch;
+    private Vector2 paperOriginalPos;
+    private bool onTelegraph;
+    public AudioSource paperAudioSourse;
+
+    private void Start()
+    {
+        paperIsCatch = false;
+        paperOriginalPos = transform.position;
+        onTelegraph = false;
+        paperAudioSourse = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            paperOriginalPos = transform.position;
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+            if (hit.collider != null && hit.collider.gameObject.name == "Paper")
+                //Recordatorio, hacerlo comparando con tags
+            {
+                paperAudioSourse.Play();
+                paperIsCatch = true;
+            }
+        }
+
+        if (paperIsCatch)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = mousePos;
+            if (Input.GetMouseButtonUp(0))
+            {
+                paperAudioSourse.Play();
+                paperIsCatch = false;
+                if (onTelegraph)
+                {
+                    transform.position = paperOriginalPos;
+                }
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Telegraph")
+        {
+            onTelegraph = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Telegraph")
+        {
+            onTelegraph = false;
+        }
+    }
+}
